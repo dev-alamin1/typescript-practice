a31e8369879af766a8ad0d2822e98b18d7f2f5fc
dev-alamin1 <dev.alaminmondal@gmail.com>
2023-01-06 11:22:03 +0600

    optional property type

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -13,8 +13,6 @@ type userDemoInfo = {
 const User = ({user}:userDemoInfo) => {
     return (
         <div>
-            {/* <p>User name {props.user.name} , age {props.user.age} ,phone {props.user.phone} {props.user.isActive? 'Active' : "Not Active"}</p> */}
-
             <p>User name {user.name} , age {user.age} ,phone {user.phone} {user.isActive? 'Active' : "Not Active"}</p>
 
         </div>
